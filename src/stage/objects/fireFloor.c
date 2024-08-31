9be7502f685f377620c00c737595848c64d091e2
Fractal <marklessfractal@gmail.com>
2024-08-31 13:14:48 +1000

    fully match firefloor

--- src/stage/objects/fireFloor.c
+++ src/stage/objects/fireFloor.c
@@ -66,9 +66,8 @@ FireFloor *CreateFireFloor(MapObject *mapObject, fx32 x, fx32 y, fx32 type)
     return work;
 }
 
-NONMATCH_FUNC void FireFloor_State_Active(FireFloor *work)
+void FireFloor_State_Active(FireFloor *work)
 {
-#ifdef NON_MATCHING
     if ((work->gameWork.objWork.displayFlag & DISPLAY_FLAG_DID_FINISH) != 0)
     {
         if (work->gameWork.objWork.obj_2d->ani.work.animID == FIREFLOOR_ANI_FLAREUP_V)
@@ -84,7 +83,7 @@ NONMATCH_FUNC void FireFloor_State_Active(FireFloor *work)
     }
 
     BOOL checkRange = FALSE;
-    BOOL flag       = FALSE;
+    BOOL isImmune   = FALSE;
     u16 id          = work->gameWork.mapObject->id;
 
     if (gPlayer->characterID == CHARACTER_BLAZE || (gPlayer->playerFlag & PLAYER_FLAG_SUPERBOOST) != 0)
@@ -93,17 +92,17 @@ NONMATCH_FUNC void FireFloor_State_Active(FireFloor *work)
     }
     else
     {
-        checkRange = flag;
+        checkRange = isImmune;
     }
 
     if (checkRange)
     {
-        if (work->gameWork.objWork.position.x - FLOAT_TO_FX32(64.0) <= gPlayer->objWork.position.x
-            && gPlayer->objWork.position.x <= work->gameWork.objWork.position.x + FLOAT_TO_FX32(64.0))
+        fx32 x = work->gameWork.objWork.position.x;
+        if (x - FLOAT_TO_FX32(64.0) <= gPlayer->objWork.position.x && gPlayer->objWork.position.x <= x + FLOAT_TO_FX32(64.0))
         {
-            if (work->gameWork.objWork.position.y - FLOAT_TO_FX32(64.0) <= gPlayer->objWork.position.y
-                && gPlayer->objWork.position.y <= work->gameWork.objWork.position.y + FLOAT_TO_FX32(64.0))
-                flag = TRUE;
+            fx32 y = work->gameWork.objWork.position.y;
+            if (y - FLOAT_TO_FX32(64.0) <= gPlayer->objWork.position.y && gPlayer->objWork.position.y <= y + FLOAT_TO_FX32(64.0))
+                isImmune = TRUE;
         }
     }
 
@@ -113,7 +112,7 @@ NONMATCH_FUNC void FireFloor_State_Active(FireFloor *work)
     else
         scale = &work->gameWork.objWork.scale.x;
 
-    if (flag && (work->gameWork.objWork.obj_2d->ani.work.animID == 0 || work->gameWork.objWork.obj_2d->ani.work.animID == 3))
+    if (isImmune && (work->gameWork.objWork.obj_2d->ani.work.animID == FIREFLOOR_ANI_ACTIVE_V || work->gameWork.objWork.obj_2d->ani.work.animID == FIREFLOOR_ANI_ACTIVE_H))
     {
         if (*scale > FLOAT_TO_FX32(0.25))
         {
@@ -149,140 +148,6 @@ NONMATCH_FUNC void FireFloor_State_Active(FireFloor *work)
         if (work->gameWork.objWork.userTimer == 0)
             work->gameWork.colliders[1].flag &= ~(OBS_RECT_WORK_FLAG_40000 | OBS_RECT_WORK_FLAG_200 | OBS_RECT_WORK_FLAG_100);
     }
-#else
-    // clang-format off
-	stmdb sp!, {r4, lr}
-	mov r4, r0
-	ldr r1, [r4, #0x20]
-	tst r1, #8
-	beq _02182498
-	ldr r1, [r4, #0x128]
-	ldrh r1, [r1, #0xc]
-	cmp r1, #1
-	bne _0218247C
-	mov r1, #0
-	bl StageTask__SetAnimation
-	b _0218248C
-_0218247C:
-	cmp r1, #4
-	bne _0218248C
-	mov r1, #3
-	bl StageTask__SetAnimation
-_0218248C:
-	ldr r0, [r4, #0x20]
-	orr r0, r0, #4
-	str r0, [r4, #0x20]
-_02182498:
-	ldr r0, =gPlayer
-	ldr r1, [r4, #0x340]
-	ldr lr, [r0]
-	ldrh r1, [r1, #2]
-	ldrb r2, [lr, #0x5d0]
-	mov r0, #0
-	cmp r2, #1
-	beq _021824C4
-	ldr r2, [lr, #0x5d8]
-	tst r2, #0x80
-	beq _021824CC
-_021824C4:
-	mov r2, #1
-	b _021824D0
-_021824CC:
-	mov r2, r0
-_021824D0:
-	cmp r2, #0
-	beq _02182510
-	ldr ip, [r4, #0x44]
-	ldr r3, [lr, #0x44]
-	sub r2, ip, #0x40000
-	cmp r2, r3
-	addle r2, ip, #0x40000
-	cmple r3, r2
-	bgt _02182510
-	ldr ip, [r4, #0x48]
-	ldr r3, [lr, #0x48]
-	sub r2, ip, #0x40000
-	cmp r2, r3
-	addle r2, ip, #0x40000
-	cmple r3, r2
-	movle r0, #1
-_02182510:
-	add r1, r1, #0xf20
-	add r1, r1, #0xf000
-	mov r1, r1, lsl #0x10
-	mov r1, r1, lsr #0x10
-	cmp r1, #1
-	addls r1, r4, #0x3c
-	addhi r1, r4, #0x38
-	cmp r0, #0
-	beq _021825A0
-	ldr r0, [r4, #0x128]
-	ldrh r0, [r0, #0xc]
-	cmp r0, #0
-	cmpne r0, #3
-	bne _021825A0
-	ldr r0, [r1]
-	cmp r0, #0x400
-	ble _02182568
-	sub r0, r0, #0x200
-	str r0, [r1]
-	cmp r0, #0x400
-	movlt r0, #0x400
-	strlt r0, [r1]
-_02182568:
-	mov r1, #0
-	mov r2, r1
-	add r0, r4, #0x258
-	bl ObjRect__SetAttackStat
-	ldr r1, =0x0000FFFE
-	add r0, r4, #0x258
-	mov r2, #0
-	bl ObjRect__SetDefenceStat
-	ldr r0, =FireFloor_OnDefend
-	str r0, [r4, #0x27c]
-	ldr r0, [r4, #0x270]
-	orr r0, r0, #0x400
-	str r0, [r4, #0x270]
-	b _021825F8
-_021825A0:
-	ldr r0, [r1]
-	cmp r0, #0x1000
-	bge _021825C0
-	add r0, r0, #0x200
-	str r0, [r1]
-	cmp r0, #0x1000
-	movgt r0, #0x1000
-	strgt r0, [r1]
-_021825C0:
-	add r0, r4, #0x200
-	mov r1, #2
-	strh r1, [r0, #0x88]
-	mov r2, #0x40
-	ldr r1, =0x0000FFFF
-	strh r2, [r0, #0x84]
-	strh r1, [r0, #0x8a]
-	mov r2, #0xff
-	ldr r1, =FireFloor_OnHit
-	strh r2, [r0, #0x86]
-	str r1, [r4, #0x27c]
-	ldr r0, [r4, #0x270]
-	bic r0, r0, #0x400
-	str r0, [r4, #0x270]
-_021825F8:
-	ldr r0, [r4, #0x2c]
-	cmp r0, #0
-	ldmeqia sp!, {r4, pc}
-	subs r0, r0, #1
-	str r0, [r4, #0x2c]
-	ldmneia sp!, {r4, pc}
-	ldr r1, [r4, #0x270]
-	ldr r0, =0xFFFBFCFF
-	and r0, r1, r0
-	str r0, [r4, #0x270]
-	ldmia sp!, {r4, pc}
-
-// clang-format on
-#endif
 }
 
 void FireFloor_OnHit(OBS_RECT_WORK *rect1, OBS_RECT_WORK *rect2)
