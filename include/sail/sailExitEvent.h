3ef126dd030bc1d6251449d161594c571b0fa41f
Geared <114871309+Sonic-Geared@users.noreply.github.com>
2024-10-09 20:49:33 -0300

    i wish github desktop wasn't this limited
    let me upload all my stuff at once github wah :(

---
+++ include/sail/sailExitEvent.h
@@ -0,0 +1,15 @@
+#ifndef RUSH2_SAILEXITEVENT_H
+#define RUSH2_SAILEXITEVENT_H
+
+#include <global.h>
+
+// --------------------
+// FUNCTIONS
+// --------------------
+
+void SailExitEvent__Create(void);
+void SailExitEvent__Main(void);
+
+BOOL SailExitEvent__IsForceQuitting(void);
+
+#endif // RUSH2_SAILEXITEVENT_H
