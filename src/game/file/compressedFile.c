631f144edbb4fb75173b3eff59875bb889577ac1
Fractal <marklessfractal@gmail.com>
2026-02-20 19:12:44 +1100

    Fully matched CompressFile module
    Thanks mongobottles!

--- src/game/file/compressedFile.c
+++ src/game/file/compressedFile.c
@@ -9,7 +9,7 @@ enum CompressedFileFlags_
     COMPRESSEDFILE_FLAG_NONE = 0x00,
 
     COMPRESSEDFILE_FLAG_USE_8_BIT_UNITS = 1 << 0,
-    COMPRESSEDFILE_FLAG_DO_CACHE_WRITE = 1 << 1,
+    COMPRESSEDFILE_FLAG_DO_CACHE_WRITE  = 1 << 1,
 };
 typedef u8 CompressedFileFlags;
 
@@ -26,20 +26,36 @@ static void CompressedFile__HandleDecompression2(void *src, void *dst, Compresse
 static void CompressedFile__HandleDecompression(void *src, void *dst, CompressedFileFlags flags);
 
 // --------------------
+// INLINE FUNCTIONS
+// --------------------
+
+RUSH_INLINE void *PTR_XOR(void **a, void **b)
+{
+    *(u32 *)a ^= *(u32 *)b;
+    *(u32 *)b ^= *(u32 *)a;
+    *(u32 *)a = (*(u32 *)a ^ *(u32 *)b);
+    return *a;
+}
+
+// --------------------
 // FUNCTIONS
 // --------------------
 
-NONMATCH_FUNC void *CompressedFile__Decompress(void *src, void *dst, void *buffer)
+void *CompressedFile__Decompress(void *src, void *dst, void *buffer)
 {
-    // https://decomp.me/scratch/Rl1ZW -> 73.38%
-#ifdef NON_MATCHING
+    typedef union SwapBuffer_
+    {
+        size_t addr[2];
+        void *ptr[2];
+    } SwapBuffer;
+
     BOOL allocatedBuffer = FALSE;
     u32 count            = 1;
+    SwapBuffer swapBuffer;
 
-    size_t size  = CompressedFile__GetCompressedSize(src);
-    void *memory = CompressedFile__AllocateMemIfNeeded(dst, size);
+    size_t size = CompressedFile__GetCompressedSize(src);
+    dst         = CompressedFile__AllocateMemIfNeeded(dst, size);
 
-    size_t ptr[2];
     if (CompressedFile__IsVISFile(src))
     {
         count = CompressedFile__GetVisCompressCount(src);
@@ -55,136 +71,41 @@ NONMATCH_FUNC void *CompressedFile__Decompress(void *src, void *dst, void *buffe
 
     if (count == 0)
     {
-        RenderCore_CPUCopy(CompressedFile__GetCompressedDataPtr(src), memory, size);
+        RenderCore_CPUCopy(CompressedFile__GetCompressedDataPtr(src), dst, size);
     }
     else
     {
         if ((count & 1) != 0)
         {
-            CompressedFile__HandleDecompression2(CompressedFile__GetCompressedDataPtr(src), memory, 2);
-            ptr[1] = (size_t)memory;
-            ptr[0] = (size_t)buffer;
+            CompressedFile__HandleDecompression2(CompressedFile__GetCompressedDataPtr(src), dst, COMPRESSEDFILE_FLAG_DO_CACHE_WRITE);
+            swapBuffer.ptr[1] = dst;
+            swapBuffer.ptr[0] = buffer;
         }
         else
         {
-            CompressedFile__HandleDecompression2(CompressedFile__GetCompressedDataPtr(src), buffer, 2);
-            ptr[1] = (size_t)buffer;
-            ptr[0] = (size_t)memory;
+            CompressedFile__HandleDecompression2(CompressedFile__GetCompressedDataPtr(src), buffer, COMPRESSEDFILE_FLAG_DO_CACHE_WRITE);
+            swapBuffer.ptr[1] = buffer;
+            swapBuffer.ptr[0] = dst;
         }
     }
 
-    for (u32 i = count - 1; i > 0; i--)
+    if (--count)
     {
-        CompressedFile__HandleDecompression2(CompressedFile__GetCompressedDataPtr((void *)ptr[1]), (void *)ptr[0], 2);
+        void *p = swapBuffer.ptr[1];
+        do
+        {
+            CompressedFile__HandleDecompression2(CompressedFile__GetCompressedDataPtr(p), swapBuffer.ptr[0], COMPRESSEDFILE_FLAG_DO_CACHE_WRITE);
 
-        MTM_MATH_SWAP(ptr[0], ptr[1]);
+            p = PTR_XOR(&swapBuffer.ptr[1], &swapBuffer.ptr[0]);
+        } while (--count);
     }
 
     if (allocatedBuffer)
         HeapFree(HEAP_USER, buffer);
 
-    DC_StoreRange(memory, size);
+    DC_StoreRange(dst, size);
 
-    return memory;
-#else
-    // clang-format off
-	stmdb sp!, {r3, r4, r5, r6, r7, r8, r9, r10, r11, lr}
-	sub sp, sp, #8
-	mov r4, r0
-	mov r10, r1
-	mov r9, r2
-	mov r7, #0
-	mov r8, #1
-	bl CompressedFile__GetCompressedSize
-	mov r11, r0
-	mov r0, r10
-	mov r1, r11
-	bl CompressedFile__AllocateMemIfNeeded
-	mov r10, r0
-	mov r0, r4
-	bl CompressedFile__IsVISFile
-	cmp r0, #0
-	beq _0205B76C
-	mov r0, r4
-	bl CompressedFile__GetVisCompressCount
-	mov r8, r0
-	cmp r8, #1
-	bls _0205B76C
-	cmp r9, #0
-	mvnne r0, #0
-	cmpne r9, r0
-	bne _0205B76C
-	mov r0, r4
-	bl CompressedFile__GetUnknownSize
-	mov r1, r0
-	mov r0, r9
-	bl CompressedFile__AllocateMemIfNeeded
-	mov r9, r0
-	mov r7, #1
-_0205B76C:
-	cmp r8, #0
-	bne _0205B78C
-	mov r0, r4
-	bl CompressedFile__GetCompressedDataPtr
-	mov r1, r10
-	mov r2, r11
-	bl RenderCore_CPUCopy
-	b _0205B7CC
-_0205B78C:
-	tst r8, #1
-	mov r0, r4
-	beq _0205B7B4
-	bl CompressedFile__GetCompressedDataPtr
-	mov r1, r10
-	mov r2, #2
-	bl CompressedFile__HandleDecompression2
-	str r10, [sp, #4]
-	str r9, [sp]
-	b _0205B7CC
-_0205B7B4:
-	bl CompressedFile__GetCompressedDataPtr
-	mov r1, r9
-	mov r2, #2
-	bl CompressedFile__HandleDecompression2
-	str r9, [sp, #4]
-	str r10, [sp]
-_0205B7CC:
-	subs r8, r8, #1
-	beq _0205B81C
-	ldr r0, [sp, #4]
-	mov r6, #2
-	add r5, sp, #4
-	add r4, sp, #0
-_0205B7E4:
-	bl CompressedFile__GetCompressedDataPtr
-	ldr r1, [sp]
-	mov r2, r6
-	bl CompressedFile__HandleDecompression2
-	ldr r2, [r5, #0]
-	ldr r1, [sp]
-	ldr r0, [r4, #0]
-	eor r2, r2, r1
-	eor r1, r0, r2
-	eor r0, r2, r1
-	str r1, [r4]
-	str r0, [r5]
-	subs r8, r8, #1
-	bne _0205B7E4
-_0205B81C:
-	cmp r7, #0
-	beq _0205B82C
-	mov r0, r9
-	bl _FreeHEAP_USER
-_0205B82C:
-	mov r0, r10
-	mov r1, r11
-	bl DC_StoreRange
-	mov r0, r10
-	add sp, sp, #8
-	ldmia sp!, {r3, r4, r5, r6, r7, r8, r9, r10, r11, pc}
-
-// clang-format on
-#endif
+    return dst;
 }
 
 BOOL CompressedFile__IsVISFile(void *memory)
@@ -201,15 +122,15 @@ size_t CompressedFile__GetCompressedSize(void *memory)
 {
     if (CompressedFile__IsVISFile(memory))
         return ((CompressedVISFileHeader *)memory)->destSize;
-        
+
     return ((MICompressionHeader *)memory)->destSize;
 }
 
 size_t CompressedFile__GetUnknownSize(void *memory)
 {
     if (CompressedFile__IsVISFile(memory))
-        return ((CompressedVISFileHeader*)memory)->unknownSize;
-    
+        return ((CompressedVISFileHeader *)memory)->unknownSize;
+
     return 0;
 }
 
@@ -217,7 +138,7 @@ void *CompressedFile__GetCompressedDataPtr(void *memory)
 {
     void *ptr = memory;
     if (CompressedFile__IsVISFile(memory))
-        ptr = ((CompressedVISFileHeader*)memory)->data;
+        ptr = ((CompressedVISFileHeader *)memory)->data;
 
     return ptr;
 }
